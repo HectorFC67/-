55e3a4eaeb947fddbbfd5d58fc10b2556272fe07
HectorFC67 <hector.f.c@opendeusto.es>
2023-03-07 11:43:36 +0100

    Main terminado EJ13

--- EJ13/main.c
+++ EJ13/main.c
@@ -2,3 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main (void)
+{
+    char array1[] = "Hola, mundo!";
+    printf("Funcion printf: %s\n", array1);
+
+    printCaracteres(array1);
+
+    char* clonedArray = clonChar(array1);
+    printf("Cadena clonada: %s\n", clonedArray);
+
+    free(clonedArray);
+
+    char array2[] = "Estamos en C!";
+    char *array = (char*) malloc ((lenght(array1)+lenght(array2)+1)*sizeof(char));
+
+    copyCadenaCharToChar (array, array1);
+    printf("Cadena copiada: %s\n", array);
+    mixCadenasChar(array, array2);
+    printf("Cadena concatenada: %s\n", array);
+
+    return 0;
+}
