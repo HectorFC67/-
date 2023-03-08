198b0672ffb165f83a6dd59ef35e0361ca7ef570
HectorFC67 <hector.f.c@opendeusto.es>
2023-03-08 08:52:40 +0100

    Comiezo de examen 2017

---
+++ Examen_Abril_2017/main.c
@@ -0,0 +1,15 @@
+#include "persona.h"
+
+int main(void)
+{
+	Persona personas[5];
+	personas[0].nombre="Hodei"; personas[0].edad=6;
+	personas[1].nombre="Anita"; personas[1].edad=41;
+	personas[2].nombre="Aitor"; personas[2].edad=12;
+	personas[3].nombre="Idoia"; personas[3].edad=31;
+	personas[4].nombre="Maite"; personas[4].edad=24;
+
+	imprimirPersona(personas[0]);
+
+	return 0;
+}
